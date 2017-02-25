dba99c92a302dca9daac2944615bd29f037975cd
Vinod PT <subramaniabharativinod@gmail.com>
2017-02-26 03:37:31 +0530

    New html. Some cleanup.

--- app/scripts/sayHi.js
+++
@@ -1,1 +0,0 @@
-console.log("Hello, the script is loaded!");
--- server.js
+++ server.js
@@ -3,23 +3,29 @@ var fs = require("fs");
 
 http.createServer(function (request, response) {
 
-  if (request.url === "/index") {
-    sendFileContent(response, "index.html", "text/html");
-  }
-  else if (request.url === "/") {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
-  }
-  else if (/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())) {
-    sendFileContent(response, request.url.toString().substring(1), "text/javascript");
-  }
-  else if (/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())) {
-    sendFileContent(response, request.url.toString().substring(1), "text/css");
-  }
-  else {
-    console.log("Requested URL is: " + request.url);
-    response.end();
-  }
+	if(request.url === "/index"){
+		sendFileContent(response, "index.html", "text/html");
+	}
+    else if(request.url === "/joinleague"){
+		sendFileContent(response, "joinleague.html", "text/html");
+	}
+	else if(request.url === "/"){
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.js$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/javascript");
+	}
+	else if(/^\/[a-zA-Z0-9\/]*.css$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/css");
+	}
+   	else if(/^\/[a-zA-Z0-9\/]*.jpg$/.test(request.url.toString())){
+		sendFileContent(response, request.url.toString().substring(1), "text/jpg");
+	}
+    else{
+		console.log("Requested URL is: " + request.url);
+		response.end();
+	}
 }).listen(3000);
 
 function sendFileContent(response, fileName, contentType) {
