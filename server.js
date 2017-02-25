7db3c562e311f4bf234bfee62ca767793cbb9e59
Vinod PT <subramaniabharativinod@gmail.com>
2017-02-25 15:38:42 +0530

    added basic node server code

--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("Hello World");
+var http = require('http');
+var fs = require("fs");
+ 
+http.createServer(function(request, response) {
+    if(request.url === "/index"){ 
+        fs.readFile("index.html", function(err, data){
+
+            if(err){
+                response.writeHead(404);
+                response.write("Not Found!");
+            } else {
+                response.writeHead(200, {'Content-Type': 'text/html'});
+                response.write(data);
+            }
+            response.end();
+        });
+    } else{
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write('<b>Hey there!</b><br /><br />This is the default response. Requested URL is: ' + request.url);
+        response.end();
+    }
+
+
+
+
+
+}).listen(3000);
